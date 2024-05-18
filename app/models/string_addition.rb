ed1496e17ca03a725ec073bce2e464fb4fdd3af6
Foram <foramthakral.tech@gmail.com>
2024-05-18 15:11:13 +0530

    add delimeters logic to sum numbers

--- app/models/string_addition.rb
+++ app/models/string_addition.rb
@@ -3,7 +3,15 @@ class StringAddition
   def add(numbers)
     return 0 if numbers.empty? # returns 0
 
+    # to handle seperate commas in input numbers
+    delimiter = /[\n,]/
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiter = parts[0][2..-1]
+      numbers = parts[1]
+    end
+
     # sum for numbers
-    numbers.split(/[\n,]/).map(&:to_i).reduce(0, :+)
+    numbers.split(/#{delimiter}/).map(&:to_i).reduce(0, :+)
   end
 end
