1b4754ed59be39889a59a194d061f370f9f51728
Foram <foramthakral.tech@gmail.com>
2024-05-18 15:01:44 +0530

    add addition in model to sum nymbers

--- app/models/string_addition.rb
+++ app/models/string_addition.rb
@@ -1,6 +1,9 @@
 # app/models/string_calculator.rb
 class StringAddition
   def add(numbers)
-    return 0 if numbers.empty?
+    return 0 if numbers.empty? # returns 0
+
+    # sum for numbers
+    numbers.split(/[\n,]/).map(&:to_i).reduce(0, :+)
   end
 end
